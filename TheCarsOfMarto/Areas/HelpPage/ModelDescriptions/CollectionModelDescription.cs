60b60ac217b93d6024fa0bcd73b00dcae6a5db7b
DESKTOP-KFFADEQ\martin <martinkondev@abv.bg>
2017-12-21 22:42:41 +0200

    Add project files.

---
+++ TheCarsOfMarto/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TheCarsOfMarto.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TheCarsOfMarto/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TheCarsOfMarto.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
